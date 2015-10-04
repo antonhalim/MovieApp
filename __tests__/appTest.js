69d307da613a3c6fb943db919e17f60b112a251b
Anton Halim <axton_n@yahoo.com>
2015-10-04 01:54:39 -0400

    added some movies to the main page

---
+++ __tests__/appTest.js
@@ -0,0 +1,11 @@
+'use strict';
+
+describe('Initiate Jest test suite is running', function(){
+	it('expect true to be true', function(){
+		expect(true).toBe(true);
+	});
+
+	it('expect one plus two is three', function(){
+		expect(1+2).toBe(3);
+	});
+});
